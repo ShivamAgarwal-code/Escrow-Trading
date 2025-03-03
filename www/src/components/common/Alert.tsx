d754024973e0b94c82319ccceec61f34b607dacc
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2025-03-03 17:58:01 +0530

    Update Alert.tsx

--- www/src/components/common/Alert.tsx
+++ www/src/components/common/Alert.tsx
@@ -9,7 +9,7 @@ const Alert = () => (
       </button>
     </AlertDialog.Trigger>
     <AlertDialog.Portal>
-      <AlertDialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
+      <AlertDialog.Overlay className="bg-blackA6 text-black data-[state=open]:animate-overlayShow fixed inset-0" />
       <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-neutral-900 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
         <AlertDialog.Title className="text-white font-bold m-0 text-[17px]">
           Are you absolutely sure?
@@ -25,7 +25,7 @@ const Alert = () => (
             </button>
           </AlertDialog.Cancel>
           <AlertDialog.Action asChild>
-            <button className="text-white bg-red-500 hover:bg-red-600 focus:shadow-red7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+            <button className="text-white bg-red-500 text-black hover:bg-red-600 focus:shadow-red7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
               Confirm
             </button>
           </AlertDialog.Action>
